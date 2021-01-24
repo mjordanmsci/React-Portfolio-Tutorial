5b5cfce18ac8715102cc6ac81cec3fe4ccd8b13b
Matt Jordan <mjordanmsci@gmail.com>
2021-01-24 16:10:46 -0500

    Styling changes
    Created styles.js to hold general styles. Styles AboutSection, FaqSection, ServicesSections. Added font-size to h4 in GlobalStyle

--- capture/src/components/FaqSection.js
+++ capture/src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {About} from '../styles';
+import styled from 'styled-components';
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>Any Question <span class='green'>FAQ</span></h2>
       <div className="question">
         <h4>How do I start?</h4>
@@ -12,6 +14,7 @@ const FaqSection = () => {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, laudantium!
           </p>
         </div>
+      <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -21,6 +24,7 @@ const FaqSection = () => {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, laudantium!
           </p>
         </div>
+      <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different payment methods</h4>
@@ -30,6 +34,7 @@ const FaqSection = () => {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, laudantium!
           </p>
         </div>
+      <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer?</h4>
@@ -39,8 +44,40 @@ const FaqSection = () => {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, laudantium!
           </p>
         </div>
+      <div className="faq-line"></div>
       </div>
-    </div>  )
-}
+    </Faq>  )
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
--- capture/src/components/ServiceSections.js
+++ capture/src/components/ServiceSections.js
@@ -5,48 +5,79 @@ import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
+import {About, Description, Image, Hide} from '../styles';
+import styled from 'styled-components';
 
 const ServiceSections = () => {
   return (
-    <div className="services">
-      <div className="description">
-        <h2>High <span class='purple'>quality</span> services</h2>
-        <div className="cards">
-          <div className="card">
+    <Services>
+      <Description>
+        <h2>High <span>quality</span> services</h2>
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt=""/>
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt=""/>
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} alt=""/>
               <h3>Money</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt=""/>
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt=""/>
-      </div>
-    </div>
+      </Image>
+    </Services>
   )
 }
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: #fff;
+      color: #000;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServiceSections;
