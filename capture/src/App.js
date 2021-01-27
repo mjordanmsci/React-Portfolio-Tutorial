b5ae5d79bd848af26a030165c078b5793b578f63
Matt Jordan <mjordanmsci@gmail.com>
2021-01-26 21:56:01 -0500

    Nav Bar and pages
    Created and styled Navbar
    Created work and contact page
    Added content to work page
    set up react router dom to navigate links

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+// Import Global Style
 import GlobalStyle from './components/GlobalStyle'
+// Import Pages
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork';
+import Nav from './components/Nav';
+// Router
+import {Switch, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
