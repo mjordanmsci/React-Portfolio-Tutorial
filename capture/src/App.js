028c96eb7b7fe9a5986de467831a6515552df1d8
Matt Jordan <mjordanmsci@gmail.com>
2021-01-27 22:39:01 -0500

    Movie State and Movie Details
    Added content to movie details using movie state

--- capture/src/App.js
+++ capture/src/App.js
@@ -6,6 +6,7 @@ import GlobalStyle from './components/GlobalStyle'
 import ContactUs from './pages/ContactUs'
 import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
 // Router
 import {Switch, Route} from 'react-router-dom';
 
@@ -20,8 +21,11 @@ function App() {
         <Route path='/' exact>
           <AboutUs />
         </Route>
-        <Route path='/work'>
+        <Route path='/work' exact>
           <OurWork />
+        </Route>
+        <Route path='/work/:id'>
+          <MovieDetail />
         </Route>
         <Route path='/contact'>
           <ContactUs />
